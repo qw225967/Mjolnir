5f4cb7ec2feb2df39a37b59f186ccc80278f3536
qw225967 <396139919@qq.com>
2022-01-17 20:02:24 +0800

    add task pool

--- pkg/pool/taskpool/global.go
+++ pkg/pool/taskpool/global.go
@@ -10,3 +10,22 @@
 package taskpool
 
 var globalPoolInstance_ *pool
+
+func Go(tFunc taskFunc, params ...interface{}) {
+	globalPoolInstance_.Go(tFunc, params...)
+}
+
+func GetCurrentStatus() Status {
+	return globalPoolInstance_.GetCurrentStatus()
+}
+
+func KillIdleWorkers() {
+	globalPoolInstance_.KillIdleWorkers()
+}
+
+
+func Init(option *Option) error {
+	var err error
+	globalPoolInstance_, err = NewPool(&Option{})
+	return err
+}
---
+++ pkg/pool/taskpool/taskpool_test.go
@@ -0,0 +1,38 @@
+/**
+ * @author      : dog head
+ * @date        : Created in 2022/1/17
+ * @mail        : 396139919@qq.com
+ * @project     : pool
+ * @file        : taskpool_test.go
+ * @description : TODO
+ */
+
+package taskpool_test
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/Mjolnir/pkg/pool/taskpool"
+)
+
+func TestNewPoolNewPool(t *testing.T) {
+	pool,_ := taskpool.NewPool(&taskpool.Option{
+		MaxWorkerNumb: 4,
+	})
+	var sum int32
+	var wg sync.WaitGroup
+	n := 1000
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		pool.Go(func(param ...interface{}) {
+			ii := param[0].(int)
+			atomic.AddInt32(&sum, int32(ii))
+			wg.Done()
+		}, i)
+	}
+	wg.Wait()
+	fmt.Println(sum)
+}
