c6eaa958353a9feb8ea5dab98747992932d44839
qw225967 <396139919@qq.com>
2022-01-21 10:36:05 +0800

    add log model

---
+++ pkg/mjolnirlog/mjolnirlog.go
@@ -0,0 +1,10 @@
+/**
+ * @author      : dog head
+ * @date        : Created in 2022/1/21
+ * @mail        : 396139919@qq.com
+ * @project     : Mjolnir
+ * @file        : mjolnirlog.go
+ * @description : TODO
+ */
+
+package mjolnirlog
