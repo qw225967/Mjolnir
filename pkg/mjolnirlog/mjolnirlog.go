ee77afd12a9130297ad78671e49147b9d31e6a18
qw225967 <396139919@qq.com>
2022-02-08 22:04:50 +0800

    add mjolnirlog logo string

--- pkg/mjolnirlog/mjolnirlog.go
+++ pkg/mjolnirlog/mjolnirlog.go
@@ -9,6 +9,8 @@
 
 package mjolnirlog
 
+import "fmt"
+
 const (
 	LevelInfo    = "Info"
 	LevelTrace   = "Trace"
@@ -19,4 +21,46 @@ const (
 )
 
 type logger struct {
+}
+
+func PrintLogo() {
+	str := `
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++                                                      +
++                                 ;.; G                +
++                                ,,:iG G               +
++                               ,tC   , C              +
++                              ,fC    L  C             +
++                             ;fL     iL  G            +
++                            :fi       8   G           +
++                           if          Coio           +
++                          if                          +
++                        .1L                           +
++                       :fL                            +
++            t1it,     :LL                             +
++           LGf::C;1t .tL                              +
++          tG1::;;;G;:1f;                              +
++         GL,;;;;;iiii:8G:if                           +
++         t:iiiiiiiiiiii;;;:ii                         +
++           tt;i;iii;;;i;;iLGi                         +
++              ;fi;;;;;;;;GG,                          +
++                  ff:;:;GL                            +
++                      LL1                             +
++                            ;                         +
++                            @t          GG            +
++                            @t                        +
++   f@@8   0@@  @@C          @t         t@@ to    or   +
++   G8@@: t8@@   G:   L0L    @t ;0 L0C   18  0GGGrr    +
++   8C @@ @ @@   @L @@   @@  @t 1@:  @@  @@  @@        +
++   @L  @@  @@   @L @8   0@  @t 1@.  @@  @@  @@        +
++  C@@:    ,@@@  @L  @@f@@  0@@ @@0 .@@1i@@ t@@        +
++                @L                                    +
++                @L                                    +
++             oGGG                                     +
++                                                      +
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++`
+
+
+	fmt.Println(str)
+
 }
---
+++ pkg/mjolnirlog/mjolnirlog_test.go
@@ -0,0 +1,18 @@
+/*******************************************************
+ * @author      : dog head
+ * @date        : Created in 2022/2/8
+ * @mail        : 396139919@qq.com
+ * @project     : Mjolnir
+ * @file        : mjolnirlog_test.go
+ * @description : TODO
+ *******************************************************/
+
+package mjolnirlog
+
+import (
+	"testing"
+)
+
+func TestPrintLogo(t *testing.T) {
+	PrintLogo()
+}
