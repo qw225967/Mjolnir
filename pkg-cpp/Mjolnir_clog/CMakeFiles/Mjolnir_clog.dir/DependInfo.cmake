e63057f86dcad054afc4254fb443172d3170a62f
qw225967 <396139919@qq.com>
2022-07-29 16:29:45 +0800

    fix namespace

---
+++ pkg-cpp/Mjolnir_clog/CMakeFiles/Mjolnir_clog.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/admin/go/src/github.com/Mjolnir/pkg-cpp/Mjolnir_clog/Buffer.cpp" "CMakeFiles/Mjolnir_clog.dir/Buffer.cpp.o" "gcc" "CMakeFiles/Mjolnir_clog.dir/Buffer.cpp.o.d"
+  "/Users/admin/go/src/github.com/Mjolnir/pkg-cpp/Mjolnir_clog/Mjolnir_clog.cpp" "CMakeFiles/Mjolnir_clog.dir/Mjolnir_clog.cpp.o" "gcc" "CMakeFiles/Mjolnir_clog.dir/Mjolnir_clog.cpp.o.d"
+  "/Users/admin/go/src/github.com/Mjolnir/pkg-cpp/Mjolnir_clog/test.cpp" "CMakeFiles/Mjolnir_clog.dir/test.cpp.o" "gcc" "CMakeFiles/Mjolnir_clog.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pkg-cpp/Mjolnir_clog/CMakeFiles/Mjolnir_clog.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mjolnir_clog.dir/Buffer.cpp.o"
+  "CMakeFiles/Mjolnir_clog.dir/Buffer.cpp.o.d"
+  "CMakeFiles/Mjolnir_clog.dir/Mjolnir_clog.cpp.o"
+  "CMakeFiles/Mjolnir_clog.dir/Mjolnir_clog.cpp.o.d"
+  "CMakeFiles/Mjolnir_clog.dir/test.cpp.o"
+  "CMakeFiles/Mjolnir_clog.dir/test.cpp.o.d"
+  "Mjolnir_clog"
+  "Mjolnir_clog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mjolnir_clog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
