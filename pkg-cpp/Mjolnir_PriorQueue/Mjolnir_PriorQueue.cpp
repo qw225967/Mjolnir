ad8aa84762e4e077fc58c6adaabcf0cc9c074113
qw225967 <396139919@qq.com>
2022-09-16 17:50:57 +0800

    [上线] 修复url超时问题

--- pkg-cpp/Mjolnir_PriorQueue/Mjolnir_PriorQueue.cpp
+++ pkg-cpp/Mjolnir_PriorQueue/Mjolnir_PriorQueue.cpp
@@ -12,7 +12,7 @@
 #include <iostream>
 
 namespace mjolnir {
-  constexpr size_t MaxMjolnirPriorityQueueSize = 3;
+  constexpr size_t MaxMjolnirPriorityQueueSize = 100;
 
   MjolnirPriorityQueue::MjolnirPriorityQueue() {}
   MjolnirPriorityQueue::~MjolnirPriorityQueue() {}
@@ -30,7 +30,7 @@ namespace mjolnir {
     }
   }
 
- void MjolnirPriorityQueue::DelListNode(ElementPtr elementPtr) {
+  void MjolnirPriorityQueue::DelListNode(ElementPtr elementPtr) {
     if (elementPtr.get()) {
       // 删除的是中间元素
       if (elementPtr->pre.get() && elementPtr->next.get()) {
@@ -67,35 +67,48 @@ namespace mjolnir {
     return res;
   }
 
-  MjolnirPriorityQueue::ElementPtr MjolnirPriorityQueue::QueuePushHeap(Element element) {
+//  MjolnirPriorityQueue::ElementPtr MjolnirPriorityQueue::QueuePushHeap(Element element) {
+//    ElementPtr e = std::make_shared<Element>(element);
+//
+//    element_queue_.push_back(e);
+//
+//    int p, i = element_queue_.size() - 1;
+//    while (i > 0) {
+//      // 父亲节点的编号
+//      p = (i - 1) / 2;
+//
+//      if ((*element_queue_[p]) < *e) break;
+//      //把父亲的节点放下来
+//      std::swap((element_queue_[p]), (element_queue_[i]));
+//      element_queue_[i]->index = i;
+//      element_queue_[p]->index = p;
+//
+//      //当前节点更新
+//      i = p;
+//    }
+//    element_queue_[i] = e;
+//    element_queue_[i]->index = i;
+//
+//    // 保证儿子有序
+//    int a = i * 2 + 1, b = i * 2 + 2;
+//    if (b < element_queue_.size() && *element_queue_[b] < *element_queue_[a]) {
+//      std::swap(element_queue_[b], element_queue_[a]);
+//      element_queue_[b]->index = b;
+//      element_queue_[a]->index = a;
+//    }
+//
+//    return e;
+//  }
+MjolnirPriorityQueue::ElementPtr MjolnirPriorityQueue::QueuePushHeap(Element element) {
     ElementPtr e = std::make_shared<Element>(element);
 
     element_queue_.push_back(e);
+    int i, j, gap, n = element_queue_.size();
 
-    int p, i =  element_queue_.size() - 1;
-    while (i > 0){
-      // 父亲节点的编号
-      p = (i - 1)/2;
-
-      if ((*element_queue_[p]) < *e) break;
-      //把父亲的节点放下来
-      std::swap((element_queue_[p]), (element_queue_[i]));
-      element_queue_[i]->index = i;
-      element_queue_[p]->index = p;
-
-      //当前节点更新
-      i = p;
-    }
-    element_queue_[i] = e;
-    element_queue_[i]->index = i;
-
-    // 保证儿子有序
-    int a = i*2+1, b = i*2+2;
-    if (b < element_queue_.size() && *element_queue_[b] < *element_queue_[a]) {
-      std::swap(element_queue_[b], element_queue_[a]);
-      element_queue_[b]->index = b;
-      element_queue_[a]->index = a;
-    }
+    for (gap = n / 2; gap > 0; gap /= 2)
+      for (i = gap; i < n; i++)
+        for (j = i - gap; j >= 0 && *element_queue_[j + gap] < *element_queue_[j]; j -= gap)
+          std::swap(element_queue_[j], element_queue_[j + gap]);
 
     return e;
   }
@@ -103,142 +116,139 @@ namespace mjolnir {
   MjolnirPriorityQueue::ElementPtr MjolnirPriorityQueue::QueuePopHeap() {
     ElementPtr res;
 
-    // step 1: 处理空值、剩余两个值（使用i*+1性质，所以最后两个无法区分大小）
-    if (element_queue_.empty()) { return res; }
-    if (element_queue_.size() <= 2) {
-      if (element_queue_.size() == 2 && *element_queue_[1] < *element_queue_[0]) {
-        std::swap(element_queue_[0], element_queue_[1]);
-        element_queue_[0]->index = 0;
-        element_queue_[1]->index = 1;
-      }
-      res = element_queue_[0];
-      element_queue_.erase(element_queue_.begin());
-      return res;
-    }
-
-    // step 2: 大于2个数据时可以根据i*2+1的性质进行计算
-    ElementPtr ret = element_queue_[0]; //根，最小值
-    int preEnd = element_queue_.size() - 1;
-    ElementPtr e = element_queue_[preEnd];
-    //最后一个节点， 放到根的数值
-    std::swap(element_queue_[0], element_queue_[preEnd]);
-    element_queue_.erase(element_queue_.end() - 1);
-
-    int i = 0, end = element_queue_.size();
-    //从根向下交换
-    while (i*2+1 < end){
-      int a = i*2+1, b = i*2+2; //两个儿子的位置
-      if (b < end && *element_queue_[b] < *element_queue_[a]) a = b;//保证 最小的儿子
-
-      if (*element_queue_[a] <= *e) {
-        //把儿子的值提上来
-        std::swap(element_queue_[i], element_queue_[a]);
-        element_queue_[i]->index = i;
-        element_queue_[a]->index = a;
-        i = a;
-      } else {
-        break;  //如果没有大小颠倒 退出
-      }
-    }
-    element_queue_[i] = e; //最终在找到的位置 i 填入 x；
-    element_queue_[i]->index = i;
-
-    i = element_queue_.size() - 1;
-    int p;
-    while (end > 0){
-      // 父亲节点的编号
-      p = (i - 1)/2;
-
-      if ((*element_queue_[p]) < *e) break;
-      //把父亲的节点放下来
-      std::swap((element_queue_[p]), (element_queue_[i]));
-      element_queue_[i]->index = i;
-      element_queue_[p]->index = p;
-
-      //当前节点更新
-      i = p;
-    }
-
-
-//    // 保证插入位置的儿子有序
-//    int a = i*2+1, b = i*2+2;
-//    if (b < element_queue_.size() && *element_queue_[b] < *element_queue_[a]) {
-//      std::swap(element_queue_[b], element_queue_[a]);
-//      element_queue_[b]->index = b;
-//      element_queue_[a]->index = a;
-//    }
-//
-//    // 保证根的儿子有序
-//    a = 1, b = 2;
-//    if (b < element_queue_.size() && *element_queue_[b] < *element_queue_[a]) {
-//      std::swap(element_queue_[b], element_queue_[a]);
-//      element_queue_[b]->index = b;
-//      element_queue_[a]->index = a;
-//    }
-
-    // 剩余两个元素时，保证元素有序
-    if (element_queue_.size() == 2 && *element_queue_[1] < *element_queue_[0]) {
-      std::swap(element_queue_[1], element_queue_[0]);
-      element_queue_[1]->index = 1;
-      element_queue_[0]->index = 0;
-    }
-
-    res = ret;
+    res = element_queue_[0];//根，最小值
+    element_queue_.erase(element_queue_.begin());
 
     return res;
   }
 
   void MjolnirPriorityQueue::QueueDelNode(Element element) {
-    // step 1: 处理空值、剩余两个值（使用i*2+1性质，所以最后两个无法区分大小）
-    if (element_queue_.empty()) { return; }
-    if (element_queue_.size() <= 2) {
-      element_queue_.erase(element_queue_.begin() + element.index - 1);
-      return;
-    }
-
-    // step 2: 处理大于2数量的删除方法
-    int i = element.index, preEnd = element_queue_.size()-1;
-    if (*element_queue_[i] <= *element_queue_[preEnd]) { // 当前节点 <= 尾结点：当前节点与父交换，仍然保证最小堆性质不变
-      std::swap(element_queue_[i], element_queue_[preEnd]);
-      element_queue_.erase(element_queue_.end() - 1);
-
-      // 只需要向下调整
-      int end = element_queue_.size()-1;
-      ElementPtr e = element_queue_[i];
-      while (i*2+1 < end){
-        int a = i*2+1, b = i*2+2;
-
-        if (b < end && *element_queue_[b] < *element_queue_[a]) a = b;
-
-        if (*element_queue_[a] >= *e) break;
-
-        std::swap(element_queue_[i], element_queue_[a]);
-        element_queue_[i]->index = i;
-        element_queue_[a]->index = a;
-        i = a;
-      }
-      element_queue_[i] = e; //最终在找到的位置 i 填入 x；
-      element_queue_[i]->index = i;
-    } else { // 当前节点 > 尾结点：当前节点与父交换，破坏了当前的堆性质
-      std::swap(element_queue_[i], element_queue_[preEnd]);
-      element_queue_.erase(element_queue_.end() - 1);
-
-      // 需要向上调整
-      ElementPtr e = element_queue_[i];
-      while (i > 0) {
-        int p = (i-1)/2;
-        if (*element_queue_[p] < *element_queue_[i]) break;
-        //把父亲的节点放下来
-        std::swap((element_queue_[p]), (element_queue_[i]));
-        element_queue_[i]->index = i;
-        element_queue_[p]->index = p;
-        //当前节点更新
-        i = p;
-      }
-      element_queue_[i] = e;
-      element_queue_[i]->index = i;
-    }
+    element_queue_.erase(element_queue_.begin() + element.index);
   }
+
+  //  MjolnirPriorityQueue::ElementPtr MjolnirPriorityQueue::QueuePopHeap() {
+  //    ElementPtr res;
+  //
+  //    // step 1: 处理空值、剩余两个值（使用i*+1性质，所以最后两个无法区分大小）
+  //    if (element_queue_.empty()) { return res; }
+  //    if (element_queue_.size() <= 2) {
+  //      if (element_queue_.size() == 2 && *element_queue_[1] < *element_queue_[0]) {
+  //        std::swap(element_queue_[0], element_queue_[1]);
+  //        element_queue_[0]->index = 0;
+  //        element_queue_[1]->index = 1;
+  //      }
+  //      res = element_queue_[0];
+  //      element_queue_.erase(element_queue_.begin());
+  //      return res;
+  //    }
+  //
+  //    // step 2: 大于2个数据时可以根据i*2+1的性质进行计算
+  //    ElementPtr ret = element_queue_[0]; //根，最小值
+  //    int preEnd = element_queue_.size() - 1;
+  //    ElementPtr e = element_queue_[preEnd];
+  //    //最后一个节点， 放到根的数值
+  //    std::swap(element_queue_[0], element_queue_[preEnd]);
+  //    element_queue_.erase(element_queue_.end() - 1);
+  //
+  //    int i = 0, end = element_queue_.size();
+  //    //从根向下交换
+  //    while (i*2+1 < end){
+  //      int a = i*2+1, b = i*2+2; //两个儿子的位置
+  //      if (b < end && *element_queue_[b] < *element_queue_[a]) a = b;//保证 最小的儿子
+  //
+  //      if (*element_queue_[a] <= *e) {
+  //        //把儿子的值提上来
+  //        std::swap(element_queue_[i], element_queue_[a]);
+  //        element_queue_[i]->index = i;
+  //        element_queue_[a]->index = a;
+  //
+  //        i = a;
+  //      } else {
+  //        break;  //如果没有大小颠倒 退出
+  //      }
+  //    }
+  //    element_queue_[i] = e; //最终在找到的位置 i 填入 x；
+  //    element_queue_[i]->index = i;
+  //
+  //    i = element_queue_.size() - 1;
+  //    int p;
+  //    while (end > 0){
+  //      // 父亲节点的编号
+  //      p = (i - 1)/2;
+  //
+  //      if ((*element_queue_[p]) < *e) break;
+  //      //把父亲的节点放下来
+  //      std::swap((element_queue_[p]), (element_queue_[i]));
+  //      element_queue_[i]->index = i;
+  //      element_queue_[p]->index = p;
+  //
+  //      //当前节点更新
+  //      i = p;
+  //    }
+  //
+  //    // 剩余两个元素时，保证元素有序
+  //    if (element_queue_.size() == 2 && *element_queue_[1] < *element_queue_[0]) {
+  //      std::swap(element_queue_[1], element_queue_[0]);
+  //      element_queue_[1]->index = 1;
+  //      element_queue_[0]->index = 0;
+  //    }
+  //
+  //    res = ret;
+  //
+  //    return res;
+  //  }
+
+//  void MjolnirPriorityQueue::QueueDelNode(Element element) {
+//    // step 1: 处理空值、剩余两个值（使用i*2+1性质，所以最后两个无法区分大小）
+//    if (element_queue_.empty()) { return; }
+//    if (element_queue_.size() <= 2) {
+//      element_queue_.erase(element_queue_.begin() + element.index - 1);
+//      return;
+//    }
+//
+//    // step 2: 处理大于2数量的删除方法
+//    int i = element.index, preEnd = element_queue_.size() - 1;
+//    if (*element_queue_[i] <= *element_queue_[preEnd]) {// 当前节点 <= 尾结点：当前节点与父交换，仍然保证最小堆性质不变
+//      std::swap(element_queue_[i], element_queue_[preEnd]);
+//      element_queue_.erase(element_queue_.end() - 1);
+//
+//      // 只需要向下调整
+//      int end = element_queue_.size() - 1;
+//      ElementPtr e = element_queue_[i];
+//      while (i * 2 + 1 < end) {
+//        int a = i * 2 + 1, b = i * 2 + 2;
+//
+//        if (b < end && *element_queue_[b] < *element_queue_[a]) a = b;
+//
+//        if (*element_queue_[a] >= *e) break;
+//
+//        std::swap(element_queue_[i], element_queue_[a]);
+//        element_queue_[i]->index = i;
+//        element_queue_[a]->index = a;
+//        i = a;
+//      }
+//      element_queue_[i] = e;//最终在找到的位置 i 填入 x；
+//      element_queue_[i]->index = i;
+//    } else {// 当前节点 > 尾结点：当前节点与父交换，破坏了当前的堆性质
+//      std::swap(element_queue_[i], element_queue_[preEnd]);
+//      element_queue_.erase(element_queue_.end() - 1);
+//
+//      // 需要向上调整
+//      ElementPtr e = element_queue_[i];
+//      while (i > 0) {
+//        int p = (i - 1) / 2;
+//        if (*element_queue_[p] < *element_queue_[i]) break;
+//        //把父亲的节点放下来
+//        std::swap((element_queue_[p]), (element_queue_[i]));
+//        element_queue_[i]->index = i;
+//        element_queue_[p]->index = p;
+//        //当前节点更新
+//        i = p;
+//      }
+//      element_queue_[i] = e;
+//      element_queue_[i]->index = i;
+//    }
+//  }
 
   void MjolnirPriorityQueue::Push(Element element) {
     if (element_queue_.size() == MaxMjolnirPriorityQueueSize) {
--- pkg-cpp/Mjolnir_PriorQueue/main.cpp
+++ pkg-cpp/Mjolnir_PriorQueue/main.cpp
@@ -4,23 +4,27 @@
 int main() {
   std::cout << "Hello, World!" << std::endl;
 
-  mjolnir::MjolnirPriorityQueue::Element a(true, 2, 10, 3185,"3185");
-  mjolnir::MjolnirPriorityQueue::Element c(true, 2, 11, 3186,"3186");
-  mjolnir::MjolnirPriorityQueue::Element e(true, 2, 12, 3187,"3187");
-  mjolnir::MjolnirPriorityQueue::Element f(true, 2, 13, 3188,"3188");
-  mjolnir::MjolnirPriorityQueue::Element b(true, 2, 14, 3189,"3189");
-  mjolnir::MjolnirPriorityQueue::Element d(true, 2, 15, 3190,"3190");
-  mjolnir::MjolnirPriorityQueue::Element g(true, 2, 16, 3191,"3191");
-  mjolnir::MjolnirPriorityQueue::Element h(true, 2, 17, 3192,"3192");
-  mjolnir::MjolnirPriorityQueue::Element i(true, 2, 18, 3193,"3193");
-  mjolnir::MjolnirPriorityQueue::Element j(true, 2, 19, 3194,"3194");
-  mjolnir::MjolnirPriorityQueue::Element k(true, 2, 20, 3195,"3195");
-  mjolnir::MjolnirPriorityQueue::Element l(true, 2, 21, 3196,"3196");
-  mjolnir::MjolnirPriorityQueue::Element m(true, 1, 22, 2509,"2509");
-  mjolnir::MjolnirPriorityQueue::Element n(true, 1, 23, 2510,"2510");
-  mjolnir::MjolnirPriorityQueue::Element o(true, 1, 24, 2511,"2511");
-  mjolnir::MjolnirPriorityQueue::Element p(true, 1, 25, 2512,"2512");
-  mjolnir::MjolnirPriorityQueue::Element q(true, 1, 26, 2513,"2513");
+  mjolnir::MjolnirPriorityQueue::Element a(true, 1, 10, 687,"687");
+  mjolnir::MjolnirPriorityQueue::Element c(true, 1, 11, 705,"705");
+  mjolnir::MjolnirPriorityQueue::Element e(true, 1, 12, 706,"706");
+  mjolnir::MjolnirPriorityQueue::Element f(true, 1, 13, 707,"707");
+  mjolnir::MjolnirPriorityQueue::Element b(true, 1, 14, 708,"708");
+  mjolnir::MjolnirPriorityQueue::Element d(true, 1, 15, 709,"709");
+  mjolnir::MjolnirPriorityQueue::Element g(true, 1, 16, 710,"710");
+  mjolnir::MjolnirPriorityQueue::Element h(true, 1, 17, 711,"711");
+  mjolnir::MjolnirPriorityQueue::Element i(true, 1, 18, 712,"712");
+  mjolnir::MjolnirPriorityQueue::Element j(true, 1, 19, 713,"713");
+  mjolnir::MjolnirPriorityQueue::Element k(true, 1, 20, 714,"714");
+  mjolnir::MjolnirPriorityQueue::Element l(true, 1, 21, 715,"715");
+  mjolnir::MjolnirPriorityQueue::Element m(true, 1, 22, 716,"716");
+  mjolnir::MjolnirPriorityQueue::Element n(true, 1, 23, 717,"717");
+  mjolnir::MjolnirPriorityQueue::Element o(true, 1, 25, 718,"718");
+  mjolnir::MjolnirPriorityQueue::Element p(true, 1, 25, 719,"719");
+  mjolnir::MjolnirPriorityQueue::Element q(true, 1, 26, 720,"720");
+  mjolnir::MjolnirPriorityQueue::Element r(true, 1, 27, 721,"721");
+  mjolnir::MjolnirPriorityQueue::Element s(true, 1, 28, 722,"722");
+  mjolnir::MjolnirPriorityQueue::Element t(true, 1, 29, 723,"723");
+  mjolnir::MjolnirPriorityQueue::Element u(true, 1, 30, 724,"724");
 
   mjolnir::MjolnirPriorityQueue que;
   que.Push(a);
@@ -30,91 +34,106 @@ int main() {
   que.Push(b);
   que.Push(d);
   que.Push(g);
-  for (auto i=0;i<3;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
-
   que.Push(h);
-  for (auto i=0;i<1;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
-
   que.Push(i);
-  for (auto i=0;i<1;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
+//  for (auto i=0;i<3;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+
   que.Push(j);
   que.Push(k);
-  for (auto i=0;i<1;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
   que.Push(l);
-  for (auto i=0;i<5;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
   que.Push(m);
-  for (auto i=0;i<5;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
   que.Push(n);
-  for (auto i=0;i<1;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
   que.Push(o);
-  for (auto i=0;i<2;i++) {
-    auto tmp = que.Pop();
-    if (tmp.isInit)
-      std::cout << "priority:" << tmp.priority
-      << ", time_stamp:" << tmp.time_stamp
-      << ", show_string:" << tmp.show_string
-      << ", seq:" << tmp.sequence
-      << ", index:" << tmp.index << std::endl;
-  }
   que.Push(p);
-
-  for (auto i=0;i<2;i++) {
+  que.Push(q);
+  que.Push(r);
+  que.Push(s);
+  que.Push(t);
+  que.Push(u);
+//
+//  que.Push(h);
+//  for (auto i=0;i<1;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//
+//  que.Push(i);
+//  for (auto i=0;i<1;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//  que.Push(j);
+//  que.Push(k);
+//  for (auto i=0;i<1;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//  que.Push(l);
+//  for (auto i=0;i<5;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//  que.Push(m);
+//  for (auto i=0;i<5;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//  que.Push(n);
+//  for (auto i=0;i<1;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//  que.Push(o);
+//  for (auto i=0;i<2;i++) {
+//    auto tmp = que.Pop();
+//    if (tmp.isInit)
+//      std::cout << "priority:" << tmp.priority
+//      << ", time_stamp:" << tmp.time_stamp
+//      << ", show_string:" << tmp.show_string
+//      << ", seq:" << tmp.sequence
+//      << ", index:" << tmp.index << std::endl;
+//  }
+//  que.Push(p);
+auto size =que.GetQueueSize()-1;
+  for (auto i=0;i<size;i++) {
     auto tmp = que.Pop();
     if (tmp.isInit)
       std::cout << "priority:" << tmp.priority
@@ -123,7 +142,7 @@ int main() {
       << ", seq:" << tmp.sequence
       << ", index:" << tmp.index << std::endl;
   }
-  que.Push(q);
+
 
   return 0;
 }
